cd13f856c98c316fa18186735238060e7c3496ff
apg <364138625@qq.com>
2019-08-19 15:44:50 +0800

    初始化项目

--- application/standalone/piaofaner/DoPython.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-/usr/bin/python3.4 /home/www/app/film/application/standalone/piaofaner/changcilist.py 814 %E4%B8%96%E7%BA%AA%E4%B8%9C%E9%83%BD%E5%9B%BD%E9%99%85%E5%BD%B1%E5%9F%8E 784 b228a7fb5d2df89ed00a6be7acda45d7TUVWXYZabcdefghi
-
-
-
-
-
-
-
