6ec30947de82e0476db3e82d9d85d27f0a989e55
apg <364138625@qq.com>
2019-08-19 15:48:56 +0800

    初始化项目

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
     //连接本地的 Redis 服务
    $redis = new \Redis();
    $redis->connect('127.0.0.1', 6379);
-   $auth = $redis->auth('jiangfengloveheibaixiaoyuan'); //设置密码
+   //$auth = $redis->auth(''); //设置密码
    //设置 redis 字符串数据
    $redis->set("1072", "0");
    // 获取存储的数据并输出
