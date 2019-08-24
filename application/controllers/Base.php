b49e4cfc59671c03b5c2588b041988d2cb2fb206
apg <364138625@qq.com>
2019-08-24 12:19:03 +0800

    月度考勤数据

--- application/controllers/Base.php
+++ application/controllers/Base.php
@@ -146,7 +146,7 @@ class BaseController extends Yaf_Controller_Abstract {
     }
     public function fileUpload()
     {
-        ini_set('memory_limit', '3072M'); // 临时设置最大内存占用为3G
+        ini_set('memory_limit', '720M'); // 临时设置最大内存占用为3G
         set_time_limit(0); // 设置脚本最大执行时间 为0 永不过期
 
         if (!isset($_FILES['file'])) {
